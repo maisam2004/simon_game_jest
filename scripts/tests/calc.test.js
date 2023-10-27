80773ca4a8e6e447dc5f1ce15921916d950f4181
maisam2004 <maisam_2004@hotmail.com>
2023-10-27 11:54:50 +0000

    Add calc files and modify jest

---
+++ scripts/tests/calc.test.js
@@ -0,0 +1,15 @@
+//const { TestScheduler } = require('jest');
+const [addition,subtraction ]= require('../calc');
+
+describe('calculator testing  ',()=>{
+    describe('Addition function',()=>{
+        test('run addtion to get 42 ',() => {
+            expect(addition(22,20)).toBe(42);
+        });
+    });
+    describe('subtraction function',()=>{
+        test('run subtraction to get 2 ',() => {
+            expect(subtraction(22,20)).toBe(2);
+        });
+    });
+});
